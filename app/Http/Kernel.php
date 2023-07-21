20ebae82b2189f3c22c64b7db43cd58dc604dc32
farhanjiratullah <farhanjiratullah794@gmail.com>
2023-07-21 17:38:32 +0700

    feat: Fixing middleware name typo in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'jt.verify' => \App\Htpp\Middleware\JWTMiddleware::class
+        'jwt.verify' => \App\Htpp\Middleware\JWTMiddleware::class
     ];
 }
